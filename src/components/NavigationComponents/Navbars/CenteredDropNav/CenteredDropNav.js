f57dd000efff04b0132699766fc7dbc9ba4a7f10
casyjs <1boris231@gmail.com>
2018-06-03 01:35:50 -0700

    attempted creating clickoutside context api wrapper / but decide to keep brainstorming

--- src/components/NavigationComponents/Navbars/CenteredDropNav/CenteredDropNav.js
+++ src/components/NavigationComponents/Navbars/CenteredDropNav/CenteredDropNav.js
@@ -8,20 +8,20 @@ import HamToXToggle from '../../NavTogglers/HamToX/HamToXToggle';
 
 class CenteredDropNav extends Component {
   state = {
-    showDropdown: false,
+    showMenu: false,
     highBreakCounter: 0,
     lowBreakCounter: 0
   };
 
-  toggleBtn = () => {
+  toggleMenu = () => {
     this.setState({
-      showDropdown: !this.state.showDropdown
+      showMenu: !this.state.showMenu
     });
   };
 
-  hideDropdown = () => {
+  hideMenu = () => {
     this.setState({
-      showDropdown: false
+      showMenu: false
     });
   };
 
@@ -33,14 +33,14 @@ class CenteredDropNav extends Component {
         if (this.state.highBreakCounter === 0) {
           this.setState({ lowBreakCounter: 0 });
           this.setState({ highBreakCounter: 1 });
-          this.hideDropdown();
+          this.hideMenu();
         }
       }
       if (window.innerWidth < 960) {
         if (this.state.lowBreakCounter === 0) {
           this.setState({ highBreakCounter: 0 });
           this.setState({ lowBreakCounter: 1 });
-          this.hideDropdown();
+          this.hideMenu();
         }
       }
     });
@@ -53,18 +53,21 @@ class CenteredDropNav extends Component {
   }
 
   handleClick = e => {
-    if (this.node.contains(e.target)) {
-      // the click is inside, continue whatever your doing
-      return;
-    }
+    // if menu is open check if click is inside or outside
+    if (this.state.showMenu) {
+      if (this.node.contains(e.target)) {
+        // the click is inside, continue whatever your doing
+        return;
+      }
 
-    // the click is outside, do something
-    this.handleClickOutSide();
+      // the click is outside, do something
+      this.handleClickOutSide();
+    }
   };
 
   handleClickOutSide = () => {
     this.setState({
-      showDropdown: false
+      showMenu: false
     });
   };
 
@@ -76,7 +79,7 @@ class CenteredDropNav extends Component {
             <Link
               className={styles.a}
               to={`${route.path}`}
-              onClick={this.hideDropdown}
+              onClick={this.hideMenu}
             >
               {route.name}
             </Link>
@@ -92,22 +95,22 @@ class CenteredDropNav extends Component {
       <div ref={node => (this.node = node)}>
         <div className={styles.centeredDropNav}>
           <div className={styles.navBar}>
-            <div className={styles.toggleBtn} onClick={this.toggleBtn}>
+            <div className={styles.toggleBtn} onClick={this.toggleMenu}>
               <HamToXToggle
-                toggleActive={this.state.showDropdown}
+                toggleActive={this.state.showMenu}
                 width="30px"
                 height="21px"
                 thickness="2px"
               />
             </div>
-            <h1 className={styles.logo} onClick={this.hideDropdown}>
+            <h1 className={styles.logo} onClick={this.hideMenu}>
               <Link className={styles.logoLink} to={this.props.routes[0].path}>
                 {this.props.logo}
               </Link>
             </h1>
           </div>
           <CSSTransition
-            in={this.state.showDropdown}
+            in={this.state.showMenu}
             timeout={700}
             classNames={{
               enter: styles.dropDownEnter,
