5e1b577524ecd13079c6bbc5d7fd87fa432f6f4c
casyjs <1boris231@gmail.com>
2018-06-08 11:49:43 -0700

    remade expanding functionality in MasterCopyPasteCard

--- src/components/Widgets/MasterCopyPasteCard.js
+++ src/components/Widgets/MasterCopyPasteCard.js
@@ -4,53 +4,42 @@ import CopyButton from './CopyButton';
 import ExpandingUsageBox from './ExpandingUsageBox';
 
 const Wrapper = styled.div`
-  transform: ${props => (props.expand ? 'translateX(-50%)' : null)};
-  position: ${props => (props.expand ? 'fixed' : null)};
-  bottom: ${props => (props.expand ? '0' : null)};
-  left: ${props => (props.expand ? '50%' : null)};
-  z-index: ${props => (props.expand ? 1000 : null)};
-  min-height: 250px;
-  display: ${props => (props.expand ? 'flex' : null)};
-  justify-content: ${props => (props.expand ? 'center' : null)};
-  align-items: ${props => (props.expand ? 'flex-end' : null)};
-  /* flex-direction: ${props => (props.expand ? 'column' : null)};
-  justify-content: ${props => (props.expand ? 'flex-end' : null)};
-  align-items: ${props => (props.expand ? 'flex-end' : null)}; */
-`;
-
-const Card = styled.div`
-  overflow: auto;
-  width: ${props =>
-    props.expand
-      ? 'calc(280px + (680 - 280) * (100vw - 320px) / (750 - 320))'
-      : null};
-  height: ${props =>
-    props.expand
-      ? 'calc(225px + (640 - 225) * (100vh - 320px) / (750 - 320))'
-      : '100%'};
-  border: 1px solid #e6e6e6;
-  background: #fff;
-  padding: 15px;
-  border-radius: 3px;
-  box-shadow: 0 10px 15px -5px rgba(0, 0, 0, 0.07);
-
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-
   &::after {
     content: '';
+    display: ${props => (props.expand ? 'block' : 'none')};
     background: rgba(0, 0, 0, 0.75);
     position: fixed;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    width: 150vw;
-    height: 150vh;
-    display: ${props => (props.expand ? 'block' : 'none')};
-    z-index: -1;
+    width: 100vw;
+    height: 100vh;
+    z-index: 998;
   }
+`;
+
+const Card = styled.div`
+  border: 1px solid #e6e6e6;
+  background: #fff;
+  padding: 15px;
+  border-radius: 3px;
+  box-shadow: 0 10px 15px -5px rgba(0, 0, 0, 0.07);
+  margin-bottom: ${props => (props.expand ? '25px' : null)};
+
+  position: ${props => (props.expand ? 'absolute' : null)};
+  top: ${props => (props.expand ? `${props.scrollPosition + 25}px` : null)};
+  left: ${props => (props.expand ? '50%' : null)};
+  transform: ${props => (props.expand ? 'translateX(-50%)' : null)};
+  z-index: ${props => (props.expand ? 1000 : null)};
+  width: ${props =>
+    props.expand
+      ? 'calc(280px + (680 - 280) * (100vw - 320px) / (750 - 320))'
+      : null};
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const ComponentWrapper = styled.div`
@@ -104,7 +93,6 @@ const ExpandHeaderWrapper = styled.div`
   justify-content: ${props => (props.expand ? 'space-between' : null)};
   align-items: ${props => (props.expand ? 'center' : null)};
   width: 100%;
-  padding: 25px 0;
 `;
 
 const ComponentName = styled.h2`
@@ -112,41 +100,64 @@ const ComponentName = styled.h2`
   color: #3a3a3a;
 `;
 
-const X = styled.h2`
+const CardX = styled.h2`
   display: ${props => (props.expand ? 'block' : 'none')};
   color: #3a3a3a;
   font-size: calc(20px + (25 - 20) * (100vmin - 320px) / (750 - 320));
   cursor: pointer;
+  padding: 15px 0 15px 15px;
+`;
+
+const FixedX = styled.h2`
+  cursor: pointer;
+  display: ${props => (props.expand ? 'flex' : 'none')};
+  justify-content: ${props => (props.expand ? 'center' : 'none')};
+  align-items: ${props => (props.expand ? 'center' : 'none')};
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  z-index: 999;
+  transform: translate(-50%, -50%);
+  font-size: calc(40px + (80 - 40) * (100vmin - 320px) / (750 - 320));
+  width: 100%;
+  height: 100%;
+  color: #dadada;
 `;
 
 class MasterCopyPasteCard extends Component {
   state = {
     cpName: '',
-    expand: false
+    expand: false,
+    scrollPosition: null
   };
 
   handleExpand = () => {
+    let scrollPosition = window.scrollY;
     this.setState({
+      scrollPosition,
       expand: !this.state.expand
     });
   };
 
   getComponentName = id => {
-    let [category, name] = id.split('/');
+    let name = id.split('/')[1];
     return name;
   };
 
   render() {
     return (
       <Wrapper expand={this.state.expand}>
-        <Card expand={this.state.expand}>
+        <Card
+          expand={this.state.expand}
+          scrollPosition={this.state.scrollPosition}
+        >
           <ExpandHeaderWrapper expand={this.state.expand}>
             <ComponentName expand={this.state.expand}>
               {this.getComponentName(this.props.id)}
             </ComponentName>
-            <X expand={this.state.expand} onClick={this.handleExpand}>
+            <CardX expand={this.state.expand} onClick={this.handleExpand}>
               X
-            </X>
+            </CardX>
           </ExpandHeaderWrapper>
           <ComponentWrapper>
             <CPComponent>{this.props.children}</CPComponent>
@@ -161,6 +172,9 @@ class MasterCopyPasteCard extends Component {
           </CopyPasteBar>
           <ExpandingUsageBox id={this.props.id} expand={this.state.expand} />
         </Card>
+        <FixedX expand={this.state.expand} onClick={this.handleExpand}>
+          X
+        </FixedX>
       </Wrapper>
     );
   }
