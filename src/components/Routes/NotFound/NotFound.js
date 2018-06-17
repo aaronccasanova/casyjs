7749ec0787d8e13432d2ee73a7d126efc5dd1646
casyjs <1boris231@gmail.com>
2018-06-16 21:46:29 -0700

    refactored NotFound to styled-components

--- src/components/Routes/NotFound/NotFound.js
+++ src/components/Routes/NotFound/NotFound.js
@@ -1,19 +1,62 @@
 import React from 'react';
-import styles from './NotFound.css';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  margin: 20px auto 40px;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+`;
+
+const Content = styled.div`
+  max-width: 1111px;
+  max-height: 60%;
+  background: black;
+  position: relative;
+`;
+
+const Image = styled.img`
+  object-fit: cover;
+  display: block;
+  width: 100%;
+  height: 100%;
+  opacity: 0.55;
+`;
+
+const MainHeading = styled.h1`
+  color: white;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  font-weight: bold;
+  top: 23%;
+`;
+
+const SubHeading = styled.h2`
+  color: white;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  white-space: nowrap;
+  top: 33%;
+`;
 
 const NotFound = () => {
   return (
-    <div className={styles.NotFound}>
-      <div className={styles.contentContainer}>
-        <img
-          className={styles.image}
+    <Wrapper>
+      <Content>
+        <Image
           src="https://images.unsplash.com/photo-1501511795728-df53825d742a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ec63be59e881ffa2ed65339c27e4db90&auto=format&fit=crop&w=634&q=80"
           alt="NotFound"
         />
-        <h1 className={styles.mainHeading}>404</h1>
-        <h2 className={styles.subHeading}>Page Not Found</h2>
-      </div>
-    </div>
+        <MainHeading>404</MainHeading>
+        <SubHeading>Page Not Found</SubHeading>
+      </Content>
+    </Wrapper>
   );
 };
 
